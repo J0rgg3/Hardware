c4a775628b8edcae1235412c35244abb0ad89ba0
j-portatil <jorgezgzlive@gmail.com>
2024-09-16 17:52:46 +0200

    Prueba lunes

---
+++ prueba.s
@@ -0,0 +1,16 @@
+	AREA Data, DATA,
+	
+		A 	DCD 	1,0,0,0,1,0,0,0,1
+		B 	DCD 	9,8,7,6,5,4,3,2,1
+		C 	DCD 	1,0,2,0,1,2,2,0,1
+		D 	DCD 	2,1,0,1,2,0,0,0,2
+
+		
+	AREA  Codigo , CODE
+	
+	
+	
+	
+	
+	
+	END
