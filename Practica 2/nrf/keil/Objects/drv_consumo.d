dc4ab8e4d00ed7c0147a510cedf81d708e6dd8e0
Jorge <114110052+J0rgg3@users.noreply.github.com>
2025-02-05 12:02:09 +0100

    Delete Practica 2 directory

--- Practica 2/nrf/keil/Objects/drv_consumo.d
+++
@@ -1,4 +0,0 @@
-.\objects\drv_consumo.o: drv_consumo.c
-.\objects\drv_consumo.o: hal_consumo.h
-.\objects\drv_consumo.o: drv_consumo.h
-.\objects\drv_consumo.o: C:\Programas-Practicas\Simuladores\Keil\ARM\ARMCC\Bin\..\include\stdint.h
